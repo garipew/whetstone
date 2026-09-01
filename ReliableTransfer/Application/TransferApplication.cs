34ca5670669e156b4a9d306ec95d9582f6f341a6
Gabriel Sa <gabriel10043@live.com>
2026-09-01 15:08:23 -0300

    add DbConnection as argument to ProcessTransfer
    update ProcessTransfer to execute Repository interactions in an atomic
    transfer.

--- ReliableTransfer/Application/TransferApplication.cs
+++ ReliableTransfer/Application/TransferApplication.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using ReliableTransfer.Domain;
 
 namespace ReliableTransfer.Application;
@@ -13,39 +14,48 @@ public class TransferApplication
 		transfers = transferRepo;
 	}
 
-	public async Task<Transfer> ProcessTransfer(Guid idempotency, int senderId, int receiverId, decimal amount)
+	public async Task<Transfer> ProcessTransfer(DbConnection db, Guid idempotency, int senderId, int receiverId, decimal amount)
 	{
-		Transfer? transfer = await transfers.GetByIdempotency(idempotency);
-		if (transfer != null) {
-			return transfer;
-		}
-
-		var sender = await users.Get(senderId);
-		var receiver = await users.Get(receiverId);
-
-		if (sender is null || receiver is null) {
-			throw new InvalidOperationException("User not found");
-		}
-
+		await using var tx = await db.BeginTransactionAsync();
 		try {
+			Transfer? transfer = await transfers.GetByIdempotency(tx, idempotency);
+			if (transfer != null) {
+				return transfer;
+			}
+
+			var sender = await users.Get(tx, senderId);
+			var receiver = await users.Get(tx, receiverId);
+
+			if (sender is null || receiver is null) {
+				throw new InvalidOperationException("User not found");
+			}
+
 			transfer = new Transfer(senderId, receiverId, amount);
 			if (!transfer.Validate()) {
 				throw new InvalidOperationException("Transfer not valid");
 			}
-			await transfers.Add(idempotency, transfer);
+			await transfers.Add(tx, idempotency, transfer);
 
 			sender.Debit(transfer.Amount);
 			receiver.Credit(transfer.Amount);
 
 			transfer.Complete();
 
-			await users.Save(sender);
-			await users.Save(receiver);
+			await users.Save(tx, sender);
+			await users.Save(tx, receiver);
 
-			await transfers.Save(transfer);
+			await transfers.Save(tx, transfer);
+			await tx.CommitAsync();
 			return transfer;
 		} catch (TransferConflictException) {
-			return (await transfers.GetByIdempotency(idempotency))!;
+			await tx.RollbackAsync();
+			await using var ttx = await db.BeginTransactionAsync();
+			var existing = (await transfers.GetByIdempotency(ttx, idempotency))!;
+			await ttx.CommitAsync();
+			return existing;
+		} catch {
+			await tx.RollbackAsync();
+			throw;
 		}
 	}
 }
